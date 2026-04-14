a06f1f078b4c868a838e688fe1bb785d2734e4df
9o64ymi <tanupat777@gmail.com>
2026-04-14 21:17:51 +0700

    fix solid queue puma env gate

--- config/puma.rb
+++ config/puma.rb
@@ -5,9 +5,9 @@
 # Puma starts a configurable number of processes (workers) and each process
 # serves each request in a thread from an internal thread pool.
 #
-# You can control the number of workers using ENV["WEB_CONCURRENCY"]. You
-# should only set this value when you want to run 2 or more workers. The
-# default is already 1.
+# You can control the number of workers using ENV["WEB_CONCURRENCY"]. Set it to
+# 0 for single mode, or 2+ for clustered mode. For this deployment we prefer
+# single mode on small web instances to reduce memory usage.
 #
 # The ideal number of threads per worker depends both on how much time the
 # application spends waiting for IO operations and on how much you wish to
@@ -15,26 +15,25 @@
 #
 # As a rule of thumb, increasing the number of threads will increase how much
 # traffic a given process can handle (throughput), but due to CRuby's
-# Global VM Lock (GVL) it has diminishing returns and will degrade the
-# response time (latency) of the application.
-#
-# The default is set to 3 threads as it's deemed a decent compromise between
-# throughput and latency for the average Rails application.
-#
-# Any libraries that use a connection pool or another resource pool should
-# be configured to provide at least as many connections as the number of
-# threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
+# Global VM Lock (GVL) it has diminishing returns and can degrade response
+# time (latency) of the application.
+threads_count = ENV.fetch("RAILS_MAX_THREADS", 3).to_i
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
 
+# Set the environment explicitly.
+environment ENV.fetch("RAILS_ENV", "development")
+
+# Use single mode by default unless WEB_CONCURRENCY is explicitly set > 0.
+workers ENV.fetch("WEB_CONCURRENCY", 0).to_i
+
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
 
-# Run the Solid Queue supervisor inside of Puma for single-server deployments
-plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"]
+# Run the Solid Queue supervisor inside Puma only when explicitly enabled.
+plugin :solid_queue if ENV["SOLID_QUEUE_IN_PUMA"] == "true"
 
 # Specify the PID file. Defaults to tmp/pids/server.pid in development.
 # In other environments, only set the PID file if requested.
